febd2acbe1815f19b13aeeaebdc49cb05c51e981
Ashlie Gray <ashliejanegray@gmail.com>
2020-11-06 18:42:34 +1000

    added links to admin and some css on show cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :check_user, only: [:admin]
   def index
   end
 
-
+private
+def check_user
+  if (user_signed_in? && !current_user.has_role?(:admin))
+    flash[:alert] = "You are not authorized to access that page"
+    redirect_to root_path
+  end
+end
 
 end
