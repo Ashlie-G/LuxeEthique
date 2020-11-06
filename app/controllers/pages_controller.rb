36694bd38b17f6cb263fb9fc8624e2203b8b227c
Ashlie Gray <ashliejanegray@gmail.com>
2020-11-06 19:37:34 +1000

    changed authenticate user so anyone can browse shop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :check_user, only: [:admin]
   def index
   end
--- app/controllers/product_listings_controller.rb
+++ app/controllers/product_listings_controller.rb
@@ -1,7 +1,7 @@
 class ProductListingsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
   before_action :set_product_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :check_user, only: [:admin]
   
 
