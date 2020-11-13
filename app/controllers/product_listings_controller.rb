b5b6666e51bdfe8bb725a602ed4f5c5e3b0e2b57
Ashlie Gray <ashliejanegray@gmail.com>
2020-11-13 11:30:17 +1000

    added env for in code for cloudinary and stripe

--- app/controllers/product_listings_controller.rb
+++ app/controllers/product_listings_controller.rb
@@ -72,7 +72,7 @@ class ProductListingsController < ApplicationController
     end
   end
   def buy
-    Stripe.api_key = "sk_test_51HgT8YJbyxCSQYdENWbWG1btsOQglHMZ7fPNVhacHQ0NrrRctyTT1ZTOzImAGctvTsQWmaz2njn7sVLMhuUyH9Yz00333Yw1og"
+    Stripe.api_key = ENV['STRIPE_API_KEY']
     session = Stripe::Checkout::Session.create({
       payment_method_types: ['card'],
       mode: 'payment',
