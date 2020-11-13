3026f3c93ed1453dcd9b2a12ea54f8bf43b787ef
Ashlie Gray <ashliejanegray@gmail.com>
2020-11-13 10:53:55 +1000

    uncommented seeds, still not going to herkou

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,77 +18,77 @@ user = User.create!(email: 'test@test.com',
 
 
 
-# ProductListing.destroy_all
-# bag_1 = ProductListing.new(name: "Pochette Metis", 
-#     brand: "Louis Vuitton", 
-#     description: "Perfect condition", 
-#     price: 2000.0, 
-#     category: "Shoulder Bag", 
-#     colour: "Other", 
-#     approved: true,
-#     user_id: 1)
-#     bag_1.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
-#     bag_1.save
-# bag_2 = ProductListing.new(name: "Chanel Classic", 
-#     brand: "Chanel", 
-#     description: "Perfect condition", 
-#     price: 1900.0, 
-#     category: "Shoulder Bag", 
-#     colour: "White",
-#     approved: false,
-#     user_id: 1)
-#     bag_2.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Chanel_Classic.jpg')), filename: 'Chanel_Classic.jpg')
-#     bag_2.save
-# bag_3 = ProductListing.new(name: "Pochette Metis", 
-#     brand: "Louis Vuitton", 
-#     description: "Perfect condition", 
-#     price: 2000.0, 
-#     category: "Shoulder Bag", 
-#     colour: "Other", 
-#     approved: true,
-#     user_id: 1)
-#     bag_3.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
-#     bag_3.save
-# bag_4 = ProductListing.new(name: "Chanel Classic", 
-#     brand: "Chanel", 
-#     description: "Perfect condition", 
-#     price: 1900.0, 
-#     category: "Shoulder Bag", 
-#     colour: "White",
-#     approved: false,
-#     user_id: 1)
-#     bag_4.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Chanel_Classic.jpg')), filename: 'Chanel_Classic.jpg')
-#     bag_4.save
-# bag_5 = ProductListing.new(name: "Pochette Metis", 
-#     brand: "Louis Vuitton", 
-#     description: "Perfect condition", 
-#     price: 2000.0, 
-#     category: "Shoulder Bag", 
-#     colour: "Other", 
-#     approved: true,
-#     user_id: 1)
-#     bag_5.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
-#     bag_5.save
-# bag_6 = ProductListing.new(name: "Pochette Metis", 
-#     brand: "Louis Vuitton", 
-#     description: "Perfect condition", 
-#     price: 2000.0, 
-#     category: "Shoulder Bag", 
-#     colour: "Other", 
-#     approved: true,
-#     user_id: 1)
-#     bag_6.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
-#     bag_6.save
-# bag_7 = ProductListing.new(name: "Chanel Classic", 
-#     brand: "Chanel", 
-#     description: "Perfect condition", 
-#     price: 1900.0, 
-#     category: "Shoulder Bag", 
-#     colour: "White",
-#     approved: true,
-#     user_id: 1)
-#     bag_7.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Chanel_Classic.jpg')), filename: 'Chanel_Classic.jpg')
-#     bag_7.save
+ProductListing.destroy_all
+bag_1 = ProductListing.new(name: "Pochette Metis", 
+    brand: "Louis Vuitton", 
+    description: "Perfect condition", 
+    price: 2000.0, 
+    category: "Shoulder Bag", 
+    colour: "Other", 
+    approved: true,
+    user_id: 1)
+    bag_1.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
+    bag_1.save
+bag_2 = ProductListing.new(name: "Chanel Classic", 
+    brand: "Chanel", 
+    description: "Perfect condition", 
+    price: 1900.0, 
+    category: "Shoulder Bag", 
+    colour: "White",
+    approved: false,
+    user_id: 1)
+    bag_2.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Chanel_Classic.jpg')), filename: 'Chanel_Classic.jpg')
+    bag_2.save
+bag_3 = ProductListing.new(name: "Pochette Metis", 
+    brand: "Louis Vuitton", 
+    description: "Perfect condition", 
+    price: 2000.0, 
+    category: "Shoulder Bag", 
+    colour: "Other", 
+    approved: true,
+    user_id: 1)
+    bag_3.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
+    bag_3.save
+bag_4 = ProductListing.new(name: "Chanel Classic", 
+    brand: "Chanel", 
+    description: "Perfect condition", 
+    price: 1900.0, 
+    category: "Shoulder Bag", 
+    colour: "White",
+    approved: false,
+    user_id: 1)
+    bag_4.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Chanel_Classic.jpg')), filename: 'Chanel_Classic.jpg')
+    bag_4.save
+bag_5 = ProductListing.new(name: "Pochette Metis", 
+    brand: "Louis Vuitton", 
+    description: "Perfect condition", 
+    price: 2000.0, 
+    category: "Shoulder Bag", 
+    colour: "Other", 
+    approved: true,
+    user_id: 1)
+    bag_5.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
+    bag_5.save
+bag_6 = ProductListing.new(name: "Pochette Metis", 
+    brand: "Louis Vuitton", 
+    description: "Perfect condition", 
+    price: 2000.0, 
+    category: "Shoulder Bag", 
+    colour: "Other", 
+    approved: true,
+    user_id: 1)
+    bag_6.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
+    bag_6.save
+bag_7 = ProductListing.new(name: "Chanel Classic", 
+    brand: "Chanel", 
+    description: "Perfect condition", 
+    price: 1900.0, 
+    category: "Shoulder Bag", 
+    colour: "White",
+    approved: true,
+    user_id: 1)
+    bag_7.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Chanel_Classic.jpg')), filename: 'Chanel_Classic.jpg')
+    bag_7.save
    
 
 # image: URI.open("https://res.cloudinary.com/ashlii/image/upload/v1604746374/9rwkf6dcii7blhvp90ebpl25d5ce.png"),
