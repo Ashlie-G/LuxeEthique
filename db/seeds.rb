3533c577a77df421a36a5ebedb25358c8ca532b7
Ashlie Gray <ashliejanegray@gmail.com>
2020-11-16 15:32:34 +1000

    changes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ bag_1 = ProductListing.new(name: "Pochette Metis",
     approved: true,
     user_id: 1)
     bag_1.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/lv_purse.jpg')), filename: 'lv_purse.jpg')
-    bag_1.save
+    bag_1.save!
 bag_2 = ProductListing.new(name: "Chanel Classic", 
     brand: "Chanel", 
     description: "Perfect condition", 
@@ -40,7 +40,7 @@ bag_2 = ProductListing.new(name: "Chanel Classic",
     approved: false,
     user_id: 1)
     bag_2.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Chanel_Classic.jpg')), filename: 'Chanel_Classic.jpg')
-    bag_2.save
+    bag_2.save!
 bag_3 = ProductListing.new(name: "Pochette Metis", 
     brand: "Louis Vuitton", 
     description: "Perfect condition", 
