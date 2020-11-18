186002c0b0fc2d2c87ecf87f142ea7ee935c76a8
Ashlie Gray <ashliejanegray@gmail.com>
2020-11-18 18:51:22 +1000

    changed success method

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,11 +4,8 @@ class PaymentsController < ApplicationController
   def success
     @product_listing = ProductListing.find_by(params[:id])
     @buyer = current_user
-    @order = Payment.create!(user_id: @buyer.id, buyer_email: @buyer.email, seller_email: @product_listing.user.email, product_listing_id: @product_listing.id, amount: @product_listing.price)
-    
-    status = @product_listing
-    status.update(approved: false)
-    status.save
+    @order = Payment.create(user_id: @buyer.id, buyer_email: @buyer.email, seller_email: @product_listing.user.email, product_listing_id: @product_listing.id, amount: @product_listing.price)
+    @product_listing.update_attribute(:approved, false)
 
   end
 
