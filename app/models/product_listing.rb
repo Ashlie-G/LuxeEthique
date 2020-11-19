ca14b01008cd6de91c61f7b202291aa494244476
Ashlie Gray <ashliejanegray@gmail.com>
2020-11-19 15:59:00 +1000

    last few validations before submission

--- app/models/product_listing.rb
+++ app/models/product_listing.rb
@@ -2,7 +2,10 @@ class ProductListing < ApplicationRecord
   belongs_to :user
   has_one_attached :image, dependent: :purge
   has_one :payment, dependent: :destroy
-  validates :name, :brand, :description, :price, :category, :colour, :image,  presence: true
+  validates :name, :brand, :description, :category, :colour, :image,  presence: true
+  validates :description, length: { maximum: 500, too_long: "#{count} characters is the maximum allowed"}, presence: true
+  validates :price, numericality: 
+            { minimum: 0, maximum: (100000), message: " must be a valid dollar amount"}, presence: true
 
   #sets pagination(limits listings on each page) and shows current product listins
   self.per_page = 4
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   rolify
-  has_many :product_listings, dependent: :delete_all
+  has_many :product_listings, dependent: :destroy
   has_one :payment, dependent: :destroy
-  validates :first_name, :last_name, :contact_number, :address, :suburb, :state, :postcode,  presence: true
+  validates :first_name, :last_name, :contact_number, :address, :suburb, length: { maximum: 50, too_long: "#{count} characters is the maximum allowed"}, presence: true
+  validates :state, presence: true
+  validates :postcode, length: { is: 4 }, presence: true
 
   
   # Include default devise modules. Others available are:
